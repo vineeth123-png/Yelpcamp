09f99393abb35112dfeeab6525b8f304ba53ecf7
Vineeth Duvvuri <vineethdvs123@gmail.com>
2021-07-11 23:00:59 +0530

    Added home page and styles to home and maps

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,5 +1,5 @@
 mapboxgl.accessToken = mapboxToken;
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
 	container: "map",
 	style: "mapbox://styles/mapbox/light-v10",
 	center: [-103.59179687498357, 40.66995747013945],
@@ -79,10 +79,10 @@ map.on("load", function () {
 
 	// inspect a cluster on click
 	map.on("click", "clusters", function (e) {
-		var features = map.queryRenderedFeatures(e.point, {
+		const features = map.queryRenderedFeatures(e.point, {
 			layers: ["clusters"],
 		});
-		var clusterId = features[0].properties.cluster_id;
+		const clusterId = features[0].properties.cluster_id;
 		map.getSource("campgrounds").getClusterExpansionZoom(
 			clusterId,
 			function (err, zoom) {
@@ -101,15 +101,8 @@ map.on("load", function () {
 	// the location of the feature, with
 	// description HTML from its properties.
 	map.on("click", "unclustered-point", function (e) {
-		var coordinates = e.features[0].geometry.coordinates.slice();
-		var mag = e.features[0].properties.mag;
-		var tsunami;
-
-		if (e.features[0].properties.tsunami === 1) {
-			tsunami = "yes";
-		} else {
-			tsunami = "no";
-		}
+		const coordinates = e.features[0].geometry.coordinates.slice();
+		const { popUpMarkup } = e.features[0].properties;
 
 		// Ensure that if the map is zoomed out such that
 		// multiple copies of the feature are visible, the
@@ -120,9 +113,7 @@ map.on("load", function () {
 
 		new mapboxgl.Popup()
 			.setLngLat(coordinates)
-			.setHTML(
-				"magnitude: " + mag + "<br>Was there a tsunami?: " + tsunami
-			)
+			.setHTML(popUpMarkup)
 			.addTo(map);
 	});
 
