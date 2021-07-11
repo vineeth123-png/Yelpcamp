10bae88caafecf047e1de7c6d719504786c6ce1e
Vineeth Duvvuri <vineethdvs123@gmail.com>
2021-07-11 12:47:32 +0530

    Added a map in show page

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -3,7 +3,7 @@ const ExpressError = require("../utils/ExpressError");
 const { cloudinary } = require("../cloudinary");
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken = process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({ accessTokens: mapBoxToken });
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 module.exports.index = async (req, res) => {
 	const campgrounds = await Campground.find({});
 	res.render("campgrounds/index", { campgrounds });
@@ -24,6 +24,7 @@ module.exports.createCampground = async (req, res) => {
 		})
 		.send();
 	const campground = new Campground(req.body.campground);
+	campground.geometry = geoData.body.features[0].geometry;
 	campground.images = req.files.map((f) => ({
 		url: f.path,
 		filename: f.filename,
