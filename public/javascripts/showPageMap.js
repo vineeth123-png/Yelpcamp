bcef9b6b88a3896508b0477c51ac7a48c655af06
Vineeth Duvvuri <vineethdvs123@gmail.com>
2021-07-11 13:11:24 +0530

    Maps now point to specific locations given in the campground

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,9 @@
 mapboxgl.accessToken = mapToken;
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
 	container: "map", // container ID
 	style: "mapbox://styles/mapbox/streets-v11", // style URL
-	center: [-74.5, 40], // starting position [lng, lat]
-	zoom: 9, // starting zoom
+	center: campground.geometry.coordinates, // starting position [lng, lat]
+	zoom: 8, // starting zoom
 });
+
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
